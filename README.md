5c10f276783e42c7b7864639cc09c6e044730294
Quinn Dougherty <qd@Quinns-iMac.local>
2020-09-20 19:57:13 -0400

    after episode 4

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Linear Algebra Done Right in Coq
+# Linear Algebra Done Right in Coq
+
+[Twitch](https://twitch.tv/quinndougherty92)
+[YouTube](https://www.youtube.com/playlist?list=PLGZRl2vU826d_IRnxA6rgTXXAG5vdx74P)
+
--- src/Classes.v
+++ src/Classes.v
@@ -1,7 +1,13 @@
 Require Import Ring.
 Require Import Field.
+Require Import Setoid.
+Require Import Classes.RelationClasses.
+Require Import Classes.Morphisms.
 
 Section VS.
+
+(*Variable (A : Type).*)
+
 Class Ring A :=
   {
   r0 : A;
@@ -12,7 +18,9 @@ Class Ring A :=
   rminus : A -> A -> A;
   isring : ring_theory r0 r1 radd rmult rminus rainv eq;
   }.
-
+(*
+Context `{Ring A}.
+*)
 Class Field A `{Ring A} :=
   {
   fdiv : A -> A -> A;
@@ -29,8 +37,8 @@ Infix "*" := rmult : field_scope.
 Notation "- k" := rainv : field_scope.
 Notation "/ k" := finv : field_scope.
 Infix "/" := fdiv : field_scope.
-
-Definition vectorspace_theory {F V : Type} `{Field F}
+(*
+Definition vectorspace_theory' {F V : Type}
            (vscalar_mult : F -> V -> V)
            (vaddition : V -> V -> V)
            (v0 : V) : Prop :=
@@ -42,161 +50,258 @@ Definition vectorspace_theory {F V : Type} `{Field F}
   (forall (a : F) (x y : V), vscalar_mult a (vaddition x y) = vaddition (vscalar_mult a x) (vscalar_mult a y)) /\
   (forall (a b : F) (x : V), vscalar_mult (radd a b) x = vaddition (vscalar_mult a x) (vscalar_mult b x))
 .
+ *)
+
+(*
+Variable (F : Type).
+Context `{Field F}.
+*)
+
+(*
+Variable (v0' : V) (vadd : V -> V -> V) (vsmult : F -> V -> V) (veq : V -> V -> Prop).
+Infix "⨥" := vadd (at level 50).
+Infix "*" := vsmult.
+Infix "==" := veq (at level 90).
+ *)
+
+Record vectorspace_theory {F V : Type} `{Field F}
+       (vadd : V -> V -> V) (vsmult : F -> V -> V) (v0 : V) (veq : V -> V -> Prop)
+  : Prop
+  := mk_vst
+       {
+         vadd_comm : forall (x y : V), veq (vadd x y) (vadd y x);
+         vadd_assoc : forall (x y z : V), veq (vadd (vadd x y) z) (vadd x (vadd y z));
+         vadd_ident : forall (x : V), veq (vadd x v0) x;
+         vadd_inv : forall (x : V), exists (w : V), veq (vadd x w) v0;
+         vsmult_ident : forall (x : V), veq (vsmult r1 x) x;
+         vdistr1 : forall (a : F) (x y : V), veq (vsmult a (vadd x y)) (vadd (vsmult a x) (vsmult a y));
+         vdistr2 : forall (a b : F) (x : V), veq (vsmult (radd a b) x) (vadd (vsmult a x) (vsmult b x));
+       }.
+
 
 Class VectorSpace F `{Field F} V :=
   {
-  vscalar_mult : F -> V -> V;
   vaddition : V -> V -> V;
+  vscalar_mult : F -> V -> V;
   v0 : V;
-  isvectorspace : vectorspace_theory vscalar_mult vaddition v0;
+  veq : V -> V -> Prop;
+  isvectorspace : vectorspace_theory vaddition vscalar_mult v0 veq;
   }.
 
+
 Declare Scope vectorspace_scope.
-Delimit Scope vectorspace_scope with vecs.
+Delimit Scope vectorspace_scope with vecsc.
 Open Scope vectorspace_scope.
 Bind Scope vectorspace_scope with VectorSpace.
-Context {F V : Type}.
+Variable (F V : Type).
 Context `{VectorSpace F V}.
 
 Definition vainv (v : V) :=
   vscalar_mult (rainv r1) v.
 
+Definition vminus (u v : V) :=
+  vaddition u (vainv v).
+
 Infix "⨥" := vaddition (at level 50) : vectorspace_scope.
 Infix "*" := vscalar_mult : vectorspace_scope.
 Notation "⨪" := vainv : vectorspace_scope.
+Infix "⨪" := vminus (at level 50) : vectorspace_scope.
+Infix "==" := veq (at level 90) : vectorspace_scope.
 
-Ltac unpack_vectorspace H1 :=
-  destruct H1;
-  inversion isvectorspace0 as [H21 [H22 [H23 [H24 [H25 [H26 H27]]]]]].
+Record vectorspace_eq_ext : Prop := mk_vseqe {
+   vadd_ext : Proper (veq ==> veq ==> veq) vaddition;
+   (*vsmult_ext : Proper (veq ==> veq) vscalar_mult;*)
+   v0_ext : Proper veq v0;
+ }.
+
+
+Theorem veq_equiv_refl : forall (v : V), v == v.
+  Admitted.
+
+Theorem veq_equiv_symm : forall (u v : V), u == v -> v == u.
+  Admitted.
+
+Theorem veq_equiv_trans : forall (u v w : V), u == v -> v == w -> u == w.
+  Admitted.
+
+Instance VEqReflexive : Reflexive veq :=
+  {
+  reflexivity := veq_equiv_refl;
+  }.
+
+Instance VEqSymmetric : Symmetric veq :=
+  {
+  symmetry := veq_equiv_symm;
+  }.
+
+Instance VEqTransitive : Transitive veq :=
+  {
+  transitivity := veq_equiv_trans
+  }.
+
+Instance VEqEquiv : Equivalence veq :=
+  {
+  Equivalence_Reflexive := VEqReflexive;
+  Equivalence_Symmetric := VEqSymmetric;
+  Equivalence_Transitive := VEqTransitive;
+  }.
+
+Theorem veq_refl : forall (v : V), v == v.
+Proof.
+  intros.
+  reflexivity.
+Qed.
+
+Theorem veq_symm : forall (u v : V), u == v -> v == u.
+Proof.
+  intros u v H2.
+  rewrite H2.
+  reflexivity.
+Qed.
+
+Theorem veq_trans : forall (u v w : V), u == v -> v == w -> u == w.
+Proof.
+  intros u v w H2 H3.
+  rewrite H2.
+  rewrite <- H3.
+  reflexivity.
+Qed.
+
+
+Add Parametric Relation : V veq
+  reflexivity proved by veq_refl
+  symmetry proved by veq_symm
+  transitivity proved by veq_trans
+    as veq_equiv_rel.
+
+
+Add Morphism vaddition with signature (veq ==> veq ==> veq) as vadd_ext1.
+  intros.
+  Admitted.
+
+
+
+
+Ltac unpack_vectorspace H :=
+  destruct H;
+  inversion isvectorspace0.
 
 Ltac unpack_ring H :=
   destruct H; inversion isring0.
 
-Ltac vby_definition H1 :=
-  unpack_vectorspace H1;
+Ltac vby_definition H :=
+  destruct H;
+  inversion isvectorspace0;
   simpl;
   easy.
 
-Lemma vaddition_assoc (u v w : V) : (u ⨥ v) ⨥ w = u ⨥ (v ⨥ w).
+Lemma vaddition_assoc (u v w : V) : (u ⨥ v) ⨥ w == u ⨥ (v ⨥ w).
 Proof.
   vby_definition H1.
 Qed.
 
-Lemma vaddition_commu (u v : V) : u ⨥ v = v ⨥ u.
+Lemma vaddition_commu (u v : V) : u ⨥ v == v ⨥ u.
 Proof.
   vby_definition H1.
 Qed.
 
-Lemma vaddition_ident (v : V) : v ⨥ v0 = v.
+Lemma vaddition_ident (v : V) : v ⨥ v0 == v.
 Proof.
   vby_definition H1.
 Qed.
 
-Lemma vaddition_inverse (v : V) : exists (w : V), v ⨥ w = v0.
+Lemma vaddition_inverse (v : V) : exists (w : V), v ⨥ w == v0.
 Proof.
   unpack_vectorspace H1.
-  specialize (H24 v).
-  destruct H24 as [w H24].
+  specialize (vadd_inv0 v).
+  destruct vadd_inv0 as [w vadd_inv0].
   exists w.
-  easy.
+  apply vadd_inv0.
 Qed.
 
-Lemma vmultiplicative_identity (v : V) : r1 * v = v.
+Lemma vmultiplicative_identity (v : V) : r1 * v == v.
 Proof.
   vby_definition H1.
 Qed.
 
-Lemma vdistributive1 (a : F) (u v : V) : a * (u ⨥ v) = a * u ⨥ a * v.
+Lemma vdistributive1 (a : F) (u v : V) : a * (u ⨥ v) == a * u ⨥ a * v.
 Proof.
   vby_definition H1.
 Qed.
 
-Lemma vdistributive2 (a b : F) (v : V) : (a + b) * v = a * v ⨥ b * v.
+Lemma vdistributive2 (a b : F) (v : V) : (a + b) * v == a * v ⨥ b * v.
 Proof.
   vby_definition H1.
 Qed.
 
 Lemma radd_inv_zero (x : F) : x + rainv x = r0.
 Proof.
-  destruct H.
-  destruct isring0.
+  unpack_ring H.
   simpl.
   rewrite (Ropp_def x).
   reflexivity.
 Qed.
 
 Theorem zero_times_vector (v : V) :
-  r0 * v = v0.
+  r0 * v == v0.
 Proof.
-  simpl.
   unpack_ring H.
   rewrite <- (Radd_0_l r0).
   simpl.
+
 Admitted.
 
-Lemma vadd_inverse_zero (v : V) : v ⨥ ⨪ v = v0.
+Lemma vadd_inverse_zero (v : V) : v ⨥ ⨪ v == v0.
 Proof.
   unfold vainv.
-  rewrite <- vmultiplicative_identity with (v:=v) at 1.
-  rewrite <- vdistributive2 with (b:=rainv r1).
+  rewrite <- vmultiplicative_identity at 1.
+  rewrite vdistributive1.
+  rewrite (vmultiplicative_identity (rainv r1 * v)).
+  rewrite <- vdistributive2.
   rewrite radd_inv_zero.
   apply zero_times_vector.
 Qed.
 
-Lemma cancel_vadditive1 (u v : V) : u = v -> u ⨥ (⨪ u) = v ⨥ (⨪ u).
+Lemma cancel_vadditive1 (u v : V) : u == v -> u ⨥ (⨪ u) == v ⨥ (⨪ u).
 Proof.
   intros H2.
-  subst.
-  reflexivity.
-Qed.
+  rewrite vadd_inverse_zero.
+ 
+Admitted.
 
-Lemma add_v_to_both_sides1 (u v : V) : forall w, u = v -> w ⨥ u = w ⨥ v.
-Proof.
-  intros; subst; reflexivity.
-Qed.
-
-Lemma add_v_to_both_sides2 (u v : V) : forall w, w ⨥ u = w ⨥ v -> u = v.
+Lemma add_to_both_sides1 (u v : V) : forall w, u == v -> w ⨥ u == w ⨥ v.
 Proof.
   intros.
-  apply add_v_to_both_sides1 with (w:= ⨪ w) in H2.
-  rewrite <- vaddition_assoc in H2.
-  rewrite vaddition_commu with (u:= ⨪ w)in H2.
-  simpl.
-Admitted.
-
-
-(*
-Lemma cancel_vadditive2 (u v w : V) : u ⨥ v = w -> u = w ⨥ (⨪ v).
-Proof.
-  intros H2.
-  subst.
-  destruct H1.
-  simpl.
-  inversion isvectorspace0 as [H21 [H22 [H23 [H24 [H25 [H26 H27]]]]]].
-  rewrite (H22 u v (⨪ v)).
-  specialize (H24 v).
-  destruct H24 as [minus_v' H24].
-  assert (G : minus_v' = ⨪ v). give_up.
-  rewrite <- G.
-  rewrite H24.
-  destruct H23 as [v0' H23].
-  specialize (H23 u).
-  rewrite (H21 u v1).
-  assert (G' : v0' = v1). give_up.
-  rewrite <- G'.
-  rewrite H23.
+  rewrite H2.
   reflexivity.
-Admitted.
-*)
-
-
-Lemma vaddition_injective (v : V) : forall u w : V, v ⨥ u = v ⨥ w -> u = w.
-Proof.
-  intros u w H2.
-  rewrite (add_v_to_both_sides2 u w v); easy.
 Qed.
 
-Theorem unique_vadditive_identity (v v0' : V) (H2 : forall u, u ⨥ v0' = u) : v0' = v0.
+Lemma add_to_both_sides2 (u v : V) : forall w, w ⨥ u == w ⨥ v -> u == v.
+Proof.
+  intros.
+  apply add_to_both_sides1 with (w:= ⨪ w) in H2.
+  rewrite <- vaddition_assoc in H2; rewrite <- vaddition_assoc in H2 at 1.
+  rewrite vaddition_commu with (u:=⨪ w) in H2.
+  rewrite vadd_inverse_zero in H2.
+  rewrite vaddition_commu in H2; rewrite vaddition_ident in H2.
+  rewrite vaddition_commu in H2; rewrite vaddition_ident in H2.
+  apply H2.
+Qed.
+
+Lemma add_to_both_sides (u v : V) : forall w, u == v <-> w ⨥ u == w ⨥ v.
+Proof.
+  split.
+  - apply add_to_both_sides1.
+  - apply add_to_both_sides2.
+Qed.
+
+Lemma vaddition_injective (v : V) : forall u w : V, v ⨥ u == v ⨥ w -> u == w.
+Proof.
+  intros u w H2.
+  rewrite (add_to_both_sides2 u w v); easy.
+Qed.
+
+Theorem unique_vadditive_identity (v v0' : V) (H2 : forall u, u ⨥ v0' == u) : v0' == v0.
 Proof.
   specialize (H2 v0).
   rewrite <- H2.
@@ -205,16 +310,19 @@ Proof.
   reflexivity.
 Qed.
 
-Lemma unique_vadditive_identity' (v v0' : V) : v ⨥ (⨪ v) = v0' -> v0' = v0.
+Lemma unique_vadditive_identity' (v v0' : V) : v ⨥ (⨪ v) == v0' -> v0' == v0.
 Proof.
   intros H2.
   subst.
-Admitted.
+  rewrite vadd_inverse_zero in H2.
+  symmetry.
+  apply H2.
+Qed.
 
 Theorem unique_vadditive_inverse
         (*        (v z' : V) : v ⨥ ((- r1) * v) = z' -> v0 = z'. *)
-        (v : V) : forall (w w' : V) (H__w : v ⨥ w = v0) (H__w' : v ⨥ w' = v0),
-    w = w'.
+        (v : V) : forall (w w' : V) (H__w : v ⨥ w == v0) (H__w' : v ⨥ w' == v0),
+    w == w'.
 Proof.
   intros.
   rewrite <- H__w' in H__w.
@@ -226,11 +334,16 @@ Qed.
 Theorem number_times_zero (a : F) :
   a * v0 = v0.
 Proof.
+  simpl.
+
 Admitted.
 
 Theorem minusone_times_vector (v : V) :
-  vscalar_mult (rainv r1) v = ⨪ v.
-Proof. Admitted.
+  (rainv r1) * v = ⨪ v.
+Proof.
+  unfold vainv.
+  reflexivity.
+Qed.
 
 Theorem vainv_involutive (v : V) :
   ⨪ (⨪ v) = v.
@@ -244,3 +357,13 @@ Proof.
 Admitted.
 
 End VS.
+
+Require Import Ensembles.
+
+Section Subspaces.
+
+
+Class Subspace {F V : Type} `{Field F} `{VectorSpace V} :=
+  {
+  additive_ident :
+  }
