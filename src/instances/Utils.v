9ca041361238afb13ee546fcd48dd902e7711e4b
Ben Siraphob <bensiraphob@gmail.com>
2021-04-06 16:47:12 +0700

    Prove various admitted lemmas

--- src/instances/Utils.v
+++ src/instances/Utils.v
@@ -1,4 +1,5 @@
 Require Import Vector.
+Require Import Coq.Program.Equality.
 
 Fixpoint zipwith {A B C : Type} {n : nat}
          (f : A -> B -> C) (v1 : t A n) (v2 : t B n) : t C n
@@ -11,19 +12,6 @@ Fixpoint zipwith {A B C : Type} {n : nat}
 
 Definition zip {A B C : Type} {n : nat} := @zipwith A B _ n pair.
 
-Lemma empty_vec_has_exactly_one {A : Type} (xs : t A 0) : xs = nil A.
-Proof.
-  Print t.
-  Admitted.
-
-Lemma zipwith_step {A B C : Type} {n : nat} :
-  forall (f : A -> B -> C) (x : A) (xs : t A n) (y : B) (ys : t B n),
-    cons C (f x y) n (zipwith f xs ys) = zipwith f (cons A x n xs) (cons B y n ys).
-Proof.
-  intros; generalize dependent ys; induction xs; intros ys; simpl; reflexivity.
-Qed.
-
-
 Theorem zipwith_commu {A B : Type} {n : nat} :
   forall (f : A -> A -> B) (xs ys : t A n)
     (H : forall a b, f a b = f b a), zipwith f xs ys = zipwith f ys xs.
@@ -31,9 +19,9 @@ Proof.
   intros.
   generalize dependent ys.
   induction xs; intros.
-  - rewrite (empty_vec_has_exactly_one ys).
-    reflexivity.
-  - simpl.
-    rewrite zipwith_step.
-    give_up.
-Admitted.
+  - now dependent destruction ys.
+  - cbn.
+    rewrite IHxs, H.
+    dependent destruction ys.
+    + reflexivity.
+Qed.
